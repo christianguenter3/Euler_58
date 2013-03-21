47967b1642bbc586d822900ac33f202b4d6b9341
masterrx <chguenter@web.de>
2013-03-21 19:59:35 +0100

    before optimization

--- Euler58.rb
+++ Euler58.rb
@@ -1,3 +1,103 @@
-class Euler58
+require 'Prime'
 
+class Spiral
+
+  def initialize(layers)
+    @layers = []
+    (1..layers).each{|x| @layers << Layer.new(x)}
+    @@singleton = self
+  end
+
+  def to_s
+    @layers.each { |x| x.each { |y| puts y} }
+  end
+
+  def get_diagonals
+    diagonals = []
+    @layers.each { |x| diagonals << x.get_diagonal }
+    return diagonals.flatten
+  end
+
+  def get_diagonal_prime_quot
+    prime_count = 0
+    diagonals = get_diagonals
+    diagonals.each { |x| prime_count += 1 if Prime.prime?(x) }
+    return 1.0 * prime_count / diagonals.size
+  end
+
+  def _add_next_layer(X)
+    @layers << Layer.new(x)
+  end
+
+  def get_max_side_len
+    return @layers[-1].get_side_length
+  end
+
+  def self.get_prime_quote_lower_10
+    index = 1
+    prime_quot = 1.0
+    while prime_quot > 0.4
+      index += 1
+      prime_quot = Spiral.new(index).get_diagonal_prime_quot
+    end
+    return @@singleton.get_max_side_len
+  end
+end
+
+class Layer
+  include Enumerable
+
+  def each(&block)
+    @layer.each{|x| yield x}
+  end
+
+  def initialize(layer_id)
+    @layer_id = layer_id
+    @layer = SpiralGenerator.new.each(layer_id)
+  end
+
+  def get_side_length
+     (@layer_id * 2) - 1
+  end
+
+  def get_diagonal
+    return [1] if @layer_id <= 1
+    diagonal = []
+    step = calc_step()
+    @layer.each_with_index do |x, index|
+      diagonal << x if (index + 1) % step == 0
+    end
+    return diagonal
+  end
+
+  def calc_step
+    2 + (@layer_id - 2) * 2
+  end
+end
+
+class SpiralGenerator
+  include Enumerable
+
+  def self.start_num(i)
+    return i if i <= 2
+    return ((i - 2) * 8) + start_num( i - 1)
+  end
+
+  def each(layer_id,&block)
+    layer = []
+    if layer_id <= 1
+      layer = [1]
+    else
+      start_num = SpiralGenerator.start_num(layer_id)
+      end_num   = start_num + (8 * (layer_id - 1)) - 1
+      (start_num..end_num).each do |x|
+        if block_given?
+          yield x
+        else
+          layer << x
+        end
+      end
+    end
+    return layer
+  end
 end
--- Euler58_Test.rb
+++ Euler58_Test.rb
@@ -1,8 +1,69 @@
 require 'test/unit'
 require_relative 'Euler58'
 
-class TestEuler58 < Test::Unit::TestCase
+class TestSpiral < Test::Unit::TestCase
+
+  def test
+    puts Spiral.new(2).get_diagonal_prime_quot
+  end
+
+  def test_get_diagonals
+    assert_equal([1,3,5,7,9,13,17,21,25], Spiral.new(3).get_diagonals)
+  end
+
+  def test_diagonal_prime_quot
+    prime_quot = Spiral.new(4).get_diagonal_prime_quot
+    assert_equal(true, prime_quot < 0.62 && prime_quot > 0.61)
+  end
+
+  def test_get_prime_quote_lower_10
+    assert_equal(31,Spiral.get_prime_quote_lower_10)
+  end
 
 
+end
 
+class TestLayer < Test::Unit::TestCase
+  LAYER_TEST = { 3 => 5,
+                  5 => 9,
+                  6 => 11}
+
+  def test_get_side_length
+    LAYER_TEST.each { |input,output| assert_equal(output, Layer.new(input).get_side_length )  }
+  end
+
+  DIAGONAL = { 1 => [1],
+               2 => [3,5,7,9],
+               4 => [31,37,43,49]}
+
+  def test_diagonal
+    DIAGONAL.each { |input,output| assert_equal(output, Layer.new(input).get_diagonal)  }
+  end
+end
+
+class TestSpiralGenerator < Test::Unit::TestCase
+
+  SPIRAL_TEST = { 1 => [1],
+                  2 => [2,3,4,5,6,7,8,9],
+                  3 => [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]}
+
+
+  def test_spiral_generator
+    SPIRAL_TEST.each{|input,output| assert_equal(output, @generator.each(input).to_a) }
+  end
+
+  STARTNUM = { 1 => 1,
+               2 => 2,
+               3 => 10,
+               4 => 26,
+               5 => 50,
+               6 => 82 }
+
+  def test_startnum
+    STARTNUM.each { |input, output| assert_equal(output, SpiralGenerator.start_num(input))  }
+  end
+
+  def setup
+    @generator = SpiralGenerator.new
+  end
 end
